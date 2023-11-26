43895ea5c708585c24958bad150e9cc06a357d5a
LeXX <lexx8665@yandex.ru>
2023-11-26 05:19:30 +0500

    Сделал ДЗ

---
+++ src/Book.java
@@ -0,0 +1,36 @@
+public class Book {
+    private final String title;
+    private final Author author;
+    private int publishingYear;
+
+    public Book(String title, Author author, int publishingYear) {
+        this.title = title;
+        this.author = author;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book {" +
+                "title = '" + title + '\'' +
+                ", author.firstName = '" + author.getFirstName() + '\'' +
+                ", author.surName = '" + author.getSurName() + '\'' +
+                ", publishingYear = " + publishingYear + '}';
+    }
+}
